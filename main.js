367a59b2dad983aecb35f459f1ffe429dbcdc063
swennemans <sven.roeterdink1@gmail.com>
2015-12-15 13:01:45 +0100

    changed main.js

--- main.js
+++ main.js
@@ -2,32 +2,32 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 /* Create components with React.createClass() */
-// const MyCounterComponent = React.createClass({
-//
-//   getInitialState: function() {
-//     return {
-//       clicks: 0
-//     }
-//   },
-//
-//   handleClick: function() {
-//     console.log('This is', this)
-//     this.setState({
-//       clicks: this.state.clicks + 1
-//     })
-//   },
-//
-// 	render: function() {
-// 		return (
-//       <div>
-//         Number of clicks: { this.state.clicks }
-//         <div>
-//           <button onClick={this.handleClick}> Click me! </button>
-//         </div>
-//       </div>
-//     )
-// 	}
-// })
+const MyCounterComponent = React.createClass({
+
+  getInitialState: function() {
+    return {
+      clicks: 0
+    }
+  },
+
+  handleClick: function() {
+    console.log('This is', this)
+    this.setState({
+      clicks: this.state.clicks + 1
+    })
+  },
+
+	render: function() {
+		return (
+      <div>
+        Number of clicks: { this.state.clicks }
+        <div>
+          <button onClick={this.handleClick}> Click me! </button>
+        </div>
+      </div>
+    )
+	}
+})
 
 /* Create components with ES6 classes */
 // class MyCounterComponent extends React.Component {
